6d89c978297b72561faa2fcfe7cccff977aed509
Anna Erika Alonzo <annaerikacalonzo@iskolarngbayan.pup.edu.ph>
2026-05-25 12:40:28 +0800

    Major revisions

--- SQLITE CODES/sqlite_profiling.py
+++ SQLITE CODES/sqlite_profiling.py
@@ -8,14 +8,17 @@ cursor = conn.cursor()
 
 # Create table; VARCHAR is used for no maximum length of characters
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS Users (
-        UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-        Username VARCHAR NOT NULL UNIQUE,
-        Password VARCHAR NOT NULL,
+CREATE TABLE IF NOT EXISTS Staff (
+        StaffID INTEGER PRIMARY KEY AUTOINCREMENT,
         FirstName VARCHAR NOT NULL,
+        MiddleName VARCHAR,
         LastName VARCHAR NOT NULL,
-        HomeAddress VARCHAR NOT NULL,
-        ContactNumber VARCHAR NOT NULL UNIQUE
+        Suffix VARCHAR,
+        ContactNo VARCHAR NOT NULL UNIQUE,
+        EmailAdd VARCHAR NOT NULL UNIQUE,
+        StaffRole VARCHAR NOT NULL,
+        Username VARCHAR NOT NULL UNIQUE,
+        Password VARCHAR NOT NULL
 )
 """)
 conn.commit()
@@ -23,14 +26,17 @@ conn.commit()
 
 def signup():
     first_name = input("Enter First Name: ")
+    middle_name = input("Enter Middle Name (Optional): ")
     last_name = input("Enter Last Name: ")
+    suffix = input("Enter Suffix (Optional): ")
     contact_no = input("Enter Contact Number: ")
-    home_ad = input("Enter Home Address: ")
+    email_add = input("Enter Email Address: ")
+    staff_role = input("Enter Staff Role: ")
 
     username = input("Create Username: ")
 
     # Check if username already exists in database
-    cursor.execute("SELECT * FROM Users WHERE Username = ?", (username,))
+    cursor.execute("SELECT * FROM Staff WHERE Username = ?", (username,))
     existing_user = cursor.fetchone()
 
     if existing_user:
@@ -44,16 +50,19 @@ def signup():
 
             # Insert user into database
             cursor.execute("""
-            INSERT INTO Users
-            (Username, Password, FirstName, LastName, HomeAddress, ContactNumber)
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT INTO Staff
+            (FirstName, MiddleName, LastName, Suffix, ContactNo, EmailAdd, StaffRole, Username, Password)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
             """, (
-                username,
-                password,
                 first_name,
+                middle_name,
                 last_name,
+                suffix,
                 contact_no,
-                home_ad
+                email_add,
+                staff_role,
+                username,
+                password
             ))
 
             conn.commit()
@@ -68,7 +77,7 @@ def login():
     password = input("Enter Password: ")
 
     cursor.execute("""
-    SELECT * FROM Users
+    SELECT * FROM Staff
     WHERE Username = ? AND Password = ?
     """, (username, password))
 
@@ -83,9 +92,8 @@ def login():
 
 def show_users():
     cursor.execute("""
-    SELECT Username, Password, FirstName, LastName,
-           ContactNumber, HomeAddress
-    FROM Users
+    SELECT FirstName, MiddleName, LastName, Suffix, ContactNo, EmailAdd, StaffRole, Username, Password
+    FROM Staff
     """)
 
     users = cursor.fetchall()
@@ -94,12 +102,15 @@ def show_users():
 
     for user in users:
         print("-----------------------------------")
-        print("First Name:", user[2])
-        print("Last Name:", user[3])
+        print("First Name:", user[0])
+        print("Middle Name:", user[1])
+        print("Last Name:", user[2])
+        print("Suffix:", user[3])
         print("Contact No:", user[4])
-        print("Home Address:", user[5])
-        print("Username:", user[0])
-        print("Password:", user[1])
+        print("Email Address:", user[5])
+        print("Staff Role:", user[6])
+        print("Username:", user[7])
+        print("Password:", user[8])
 
 
 # Main Menu
